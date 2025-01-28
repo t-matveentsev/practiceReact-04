5ac753e89d6cb0a7a4215f3b98999a8e2163dd95
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-28 01:22:24 +0100

    http request

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import ApplyForm from "./components/ApplyForm/ApplyForm";
-import ControlledForm from "./components/Forms/ControlledForm";
-// import UseIdExample from "./components/UseidExample/UseidExample";
+import { useEffect, useState } from "react";
+import ArticleList from "./components/ArticleList/ArticleList";
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setIsError(false);
+        setLoading(true); // 1. Встановлюємо індикатор в true перед запитом
+        const { hits } = await fetchArticles();
+        setArticles(hits);
+      } catch {
+        setIsError(true);
+        // Тут будемо обробляти помилку
+      } finally {
+        // 2. Встановлюємо індикатор в false після запиту
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+
   return (
-    <>
-      <ApplyForm></ApplyForm>
-      {/* <UseIdExample /> */}
-      <ControlledForm />
-    </>
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {isError && <p>Something went wrong!!</p>}
+      {articles.length > 0 && <ArticleList items={articles} />}
+      <button>Load more</button>
+    </div>
   );
 }
